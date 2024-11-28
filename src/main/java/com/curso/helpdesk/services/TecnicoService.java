b48dd8e5be150be8eaf35496cbd752660eba99a3
Breno Chiori <brenochiori871@gmail.com>
2024-11-28 15:31:11 -0300

    feat: created TecnicoResource and TecnicoService

---
+++ src/main/java/com/curso/helpdesk/services/TecnicoService.java
@@ -0,0 +1,20 @@
+package com.curso.helpdesk.services;
+
+import com.curso.helpdesk.domain.Tecnico;
+import com.curso.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Integer id) {
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
