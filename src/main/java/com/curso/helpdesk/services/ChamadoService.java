a7c72f47ece53d345f88da259ef9c1723cce7678
Breno Chiori <brenochiori871@gmail.com>
2024-11-29 11:21:11 -0300

    feat: create findById Chamado

---
+++ src/main/java/com/curso/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.curso.helpdesk.services;
+
+import com.curso.helpdesk.domain.Chamado;
+import com.curso.helpdesk.repositories.ChamadoRepository;
+import com.curso.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = chamadoRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+    }
+}
