c8f877c7a19a20a22d4405912511e0d1ae1a999d
Breno Chiori <brenochiori871@gmail.com>
2024-11-29 11:44:23 -0300

    feat: create endpoint findAll Chamado

--- src/main/java/com/curso/helpdesk/services/ChamadoService.java
+++ src/main/java/com/curso/helpdesk/services/ChamadoService.java
@@ -6,16 +6,21 @@ import com.curso.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ChamadoService {
 
     @Autowired
-    private ChamadoRepository chamadoRepository;
+    private ChamadoRepository repository;
 
     public Chamado findById(Integer id) {
-        Optional<Chamado> obj = chamadoRepository.findById(id);
+        Optional<Chamado> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+    }
+
+    public List<Chamado> findAll() {
+        return repository.findAll();
     }
 }
