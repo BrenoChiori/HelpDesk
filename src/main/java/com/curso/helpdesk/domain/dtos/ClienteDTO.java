77be32fe5f0cce3994b447f78deabd327c066043
Breno Chiori <brenochiori871@gmail.com>
2024-11-29 09:40:46 -0300

    feat: create CRUD Cliente

---
+++ src/main/java/com/curso/helpdesk/domain/dtos/ClienteDTO.java
@@ -0,0 +1,102 @@
+package com.curso.helpdesk.domain.dtos;
+
+import com.curso.helpdesk.domain.Cliente;
+import com.curso.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ClienteDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    protected Integer id;
+    @NotNull(message = "O compo NOME é requerido")
+    protected String nome;
+    @NotNull(message = "O compo CPF é requerido")
+    protected String cpf;
+    @NotNull(message = "O compo EMAIL é requerido")
+    protected String email;
+    @NotNull(message = "O compo SENHA é requerido")
+    protected String senha;
+    protected Set<Integer> perfis = new HashSet<>();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    public ClienteDTO() {
+        super();
+        addPerfis(Perfil.CLIENTE);
+    }
+
+    public ClienteDTO(Cliente obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+        addPerfis(Perfil.CLIENTE);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfis(Perfil perfil) {
+        this.perfis.add(perfil.getCodigo());
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/com/curso/helpdesk/services/ClienteService.java
@@ -0,0 +1,71 @@
+package com.curso.helpdesk.services;
+
+import com.curso.helpdesk.domain.Cliente;
+import com.curso.helpdesk.domain.Pessoa;
+import com.curso.helpdesk.domain.dtos.ClienteDTO;
+import com.curso.helpdesk.repositories.ClienteRepository;
+import com.curso.helpdesk.repositories.PessoaRepository;
+import com.curso.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.curso.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository tecnicoRepository;
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public Cliente findById(Integer id) {
+        Optional<Cliente> obj = tecnicoRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id " + id));
+    }
+
+    public List<Cliente> findAll() {
+        return tecnicoRepository.findAll();
+    }
+
+    public Cliente create(ClienteDTO objDTO) {
+        objDTO.setId(null);
+        validaPorCpfEEMail(objDTO);
+        Cliente newObj = new Cliente(objDTO);
+        return tecnicoRepository.save(newObj);
+    }
+
+    public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+        objDTO.setId(id);
+        Cliente oldObj = findById(id);
+        validaPorCpfEEMail(objDTO);
+        oldObj = new Cliente(objDTO);
+        return tecnicoRepository.save(oldObj);
+    }
+
+    public void delete(Integer id) {
+        Cliente obj = findById(id);
+        if(obj.getChamados().size() > 0) {
+            throw new DataIntegrityViolationException("Cliente possui oredens de serviço e não pode ser deletado!");
+        }
+
+        tecnicoRepository.deleteById(id);
+    }
+
+    private void validaPorCpfEEMail(ClienteDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema");
+        }
+
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("Email já cadastrado no sistema");
+        }
+    }
+
+
+}
